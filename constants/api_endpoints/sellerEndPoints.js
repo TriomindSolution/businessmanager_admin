08fb34511fcb64e5541f3c455ed93d778f64579e
suparansharma <suparansharmap@gmail.com>
2024-03-06 22:19:50 +0600

    update

--- constants/api_endpoints/sellerEndPoints.js
+++ constants/api_endpoints/sellerEndPoints.js
@@ -2,6 +2,6 @@ export const SELLER_END_POINT = {
     create: () => `seller/store`,
     update: (id) => `seller/update/${id}`,
     list: (page, limit) => `/seller-list?limit=${limit}`,
-    info: (id) => `/subject/${id}`,
+    info: (id) => `seller/${id}`,
     delete:(id) => `seller/delete/${id}`,
 }   
